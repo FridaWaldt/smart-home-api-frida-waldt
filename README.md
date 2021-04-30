11392f7e3c424b90232faf7c285b1167080e6bfb
Frida Waldt <fridawaldt@gmail.com>
2021-04-30 11:37:27 +0200

    Klart

--- README.md
+++ README.md
@@ -1,2 +1,99 @@
 # smart-home-api-frida-waldt
- Inlämningsuppgift 1 Backend i NodeJS - Smart Home API
+
+Inlämningsuppgift 1 Backend i NodeJS - Smart Home API
+
+Hur man använder de olika apparaterna.
+
+Dammsugaren:
+Skriv följande i URL:en
+
+    För att starta dammsugaren
+        localhost:3000/vacuum/status/on
+
+    För att stänga av dammsugaren
+        localhost:3000/vacuum/status/off
+
+AC:
+Skriv följande i URL:en
+
+    För att starta AC
+        localhost:3000/ac/status/on
+
+    För att stänga av AC
+        localhost:3000/ac/status/off
+
+    För att ändra temperaturen
+        localhost:3000/ac/temp/(nummer)
+    (nummer) är i det här fallet de graderna du vill ha
+
+Gardinen:
+Skriv följande i URL:en
+
+    För att ta ner gardinen
+        localhost:3000/blinds/status/down
+
+    För att ta upp gardinen
+        localhost:3000/blinds/status/up
+
+Kameran:
+Skriv följande i URL:en
+
+    För att starta kameran
+        localhost:3000/cam/status/on
+
+    För att stänga av kameran
+        localhost:3000/cam/status/off
+
+Högtalaren:
+Skriv följande i URL:en
+
+    För att starta högtalaren
+        localhost:3000/speaker/status/on
+
+    För att stänga av högtalaren
+        localhost:3000/speaker/status/off
+
+Lamporna:
+Skriv följande i URL:en
+
+    Lampa 1
+        För att starta lampa 1
+            localhost:3000/lights/1/status/on
+
+        För att stänga av lampa 1
+            localhost:3000/lights/1/status/off
+
+        För att ändra ljusstyrka på lampa 1
+            localhost:3000/lights/1/brightness/(nummer)
+        (nummer) i det här fallet är siffran på ljusstyrkan du väljer själv mellan 0-1.
+
+    Lampa 2
+        För att starta lampa 2
+            localhost:3000/lights/2/status/on
+
+        För att stänga av lampa 2
+            localhost:3000/lights/2/status/off
+
+        För att ändra ljusstyrka på lampa 2
+            localhost:3000/lights/2/brightness/(nummer)
+        (nummer) i det här fallet är siffran på ljusstyrkan du väljer själv mellan 0-1.
+
+    Lampa 3
+        För att starta lampa 3
+            localhost:3000/lights/3/status/on
+
+        För att stänga av lampa 3
+            localhost:3000/lights/3/status/off
+
+        För att ändra ljusstyrka på lampa 3
+            localhost:3000/lights/3/brightness/(nummer)
+        (nummer) i det här fallet är siffran på ljusstyrkan du väljer själv mellan 0-1.
+
+Dörren:
+Skriv följande i URL:en
+
+    För att öppna dörren
+        localhost:3000/lock/status/open
+
+    För att stänga av kameran
+        localhost:3000/lock/status/locked
--- index.js
+++ index.js
@@ -8,47 +8,262 @@ app.listen(3000, () => {
 
 /* CODE YOUR API HERE */
 
-/* 
-app.get('/lampor', (req, res) => {
-    console.log(req.query)
 
-    db.get('devices')
-    .find({type: "Light", id: req.query.id })
-    .assign({ on : req.query.on }) // Turn on the device and change brightness
-    .value();
+/************ DAMMSUGAREN *************/
 
-    update();
-    res.send("Lampa tänd")
-})
- */
-
-// Dammsugaren
-
-app.get('/dammsugare/:status', (req,res) => {
+// On/off
+app.get('/vacuum/status/:status', (req,res) => {
+    //om status sätts som on i URL:en kommer dammsugaren att sättas igång med hjälp av params. Skriver man off i URL:en kommer dammsugaren att stängas av, skriver man något annat kommer man få tillbaka ett meddelande om att skriva "on" eller "off"
     if(req.params.status === "on"){
         req.params.status = true
-    } else{
+        res.send("Dammsugare på")
+    } else if(req.params.status === "off"){
         req.params.status = false
+        res.send("Dammsugare av")
+    }else{
+        res.send("Välj rätt kommando, 'on' eller 'off'")
     }
 
+    //Hämtar alla devices från vår databas "db"
     db.get('devices')
+    //Hittar det specifika ID:et "VAC1"
     .find({id: "VAC1" })
-    .assign({ on : req.params.status }) // Turn on the device 
+    //Tilldelar true/false till dammsugaren som man får från params.
+    .assign({ on : req.params.status })
+    //Value gör så vi kan läsa datan vi får tillbaka
     .value();
 
+    //Uppdaterar med den nya infon från assign
     update();
-    res.send("Dammsugare på")
 })
 
 
-// AC:n
 
-app.get('/AC', (req,res) => {
+
+/************* AC:N ************/
+
+// On/off
+
+app.get('/ac/status/:status', (req,res) => {
+    //om status sätts som on i URL:en kommer AC:n att sättas igång med hjälp av params. Skriver man off i URL:en kommer AC:n att stängas av, skriver man något annat kommer man få tillbaka ett meddelande om att skriva "on" eller "off"
+    if(req.params.status === "on"){
+        req.params.status = true
+        res.send("AC på")
+    } else if(req.params.status === "off"){
+        req.params.status = false
+        res.send("AC av")
+    } else{
+        res.send("Välj rätt kommando, 'on' eller 'off'")
+    }
+
+    //Hämtar alla devices från vår databas "db"
     db.get('devices')
+    //Hittar det specifika ID:et "AC1"
     .find({id: "AC1" })
-    .assign({ on : true }) // Turn on the device 
+     //Tilldelar true/false till AC:n som man får från params.
+    .assign({ on : req.params.status })
+    //Value gör så vi kan läsa datan vi får tillbaka
     .value();
 
+     //Uppdaterar med den nya infon från assign
     update();
-    res.send("AC på")
+})
+
+//Temp
+
+app.get('/ac/temp/:temp', (req,res) => {
+    //Hämtar alla devices från vår databas "db"
+    db.get('devices')
+    //Hittar det specifika ID:et "AC1"
+    .find({id: "AC1" })
+    //Tilldelar temperaturen från params till AC:n 
+    .assign({ temperature : req.params.temp })
+    //Value gör så vi kan läsa datan vi får tillbaka
+    .value();
+
+    //Uppdaterar med den nya infon från assign
+    update();
+
+    //Får tillbaka meddelande om att AC:n har tempen från params.
+    res.send(`AC har nu tempen ${req.params.temp} C`)
+})
+
+
+/****************** GARDINER ****************/
+
+//On/off
+
+app.get('/blinds/status/:status', (req,res) => {
+    //om status sätts som down i URL:en kommer gardinen att gå ner med hjälp av params. Skriver man up i URL:en kommer gardinen att gå upp, skriver man något annat kommer man få tillbaka ett meddelande om att skriva "down" eller "up"
+    if(req.params.status === "down"){
+        req.params.status = true
+        res.send("Rullgardiner nerdragna")
+    } else if(req.params.status === "up"){
+        req.params.status = false
+        res.send("Rullgardiner uppdragna")
+    } else{
+        res.send("Välj rätt kommando, 'down' eller 'up'")
+    }
+
+    //Hämtar alla devices från vår databas "db"
+    db.get('devices')
+    //Hittar det specifika ID:et "BLI1"
+    .find({id: "BLI1" })
+     //Tilldelar true/false till gardinen som man får från params.
+    .assign({ on : req.params.status }) 
+    //Value gör så vi kan läsa datan vi får tillbaka
+    .value();
+
+    //Uppdaterar med den nya infon från assign
+    update();
+})
+
+
+
+/****************** KAMERA ****************/
+
+//On/off
+
+app.get('/cam/status/:status', (req,res) => {
+    //om status sätts som on i URL:en kommer kameran att sättas igång med hjälp av params. Skriver man off i URL:en kommer kameran att stängas av, skriver man något annat kommer man få tillbaka ett meddelande om att skriva "on" eller "off"
+    if(req.params.status === "on"){
+        req.params.status = true
+        res.send("Kamera på")
+    } else if(req.params.status === "off"){
+        req.params.status = false
+        res.send("Kamera av")
+    } else{
+        res.send("Välj rätt kommando, 'on' eller 'off'")
+    }
+
+    //Hämtar alla devices från vår databas "db"
+    db.get('devices')
+    //Hittar det specifika ID:et "CAM1"
+    .find({ id: "CAM1" })
+    //Tilldelar true/false till kamera som man får från params.
+    .assign({ on : req.params.status }) 
+    //Value gör så vi kan läsa datan vi får tillbaka
+    .value();
+
+    //Uppdaterar med den nya infon från assign
+    update();
+})
+
+
+
+
+
+/****************** HÖGTALARE ****************/
+
+//On/off
+
+app.get('/speaker/status/:status', (req,res) => {
+    //om status sätts som on i URL:en kommer högtalaren att sättas igång med hjälp av params. Skriver man off i URL:en kommer högtalaren att stängas av, skriver man något annat kommer man få tillbaka ett meddelande om att skriva "on" eller "off"
+    if(req.params.status === "on"){
+        req.params.status = true
+        res.send("Högtalare på")
+    } else if(req.params.status === "off"){
+        req.params.status = false
+        res.send("Högtalare av")
+    } else{
+        res.send("Välj rätt kommando, 'on' eller 'off'")
+    }
+
+    //Hämtar alla devices från vår databas "db"
+    db.get('devices')
+    //Hittar det specifika ID:et "SPE1"
+    .find({id: "SPE1" })
+    //Tilldelar true/false till högtalare som man får från params.
+    .assign({ on : req.params.status })
+    //Value gör så vi kan läsa datan vi får tillbaka
+    .value();
+
+    //Uppdaterar med den nya infon från assign
+    update();
+})
+
+
+
+/****************** LAMPOR ****************/
+
+//On/off
+app.get('/lights/:number/status/:status', (req,res) => {
+    //om status sätts som on i URL:en kommer lampan att sättas igång med hjälp av params. Skriver man off i URL:en kommer lampan att stängas av, skriver man något annat kommer man få tillbaka ett meddelande om att skriva "on" eller "off"
+    if(req.params.status === "on"){
+        req.params.status = true
+        res.send(`Lampa ${req.params.number}  på`)
+    } else if(req.params.status === "off"){
+        req.params.status = false
+        res.send(`Lampa ${req.params.number} av`)
+    } else{
+        res.send("Välj rätt kommando, 'on' eller 'off'")
+    }
+
+    //vårt nummer från params som kommer sättas tillsammans med "LIG" för att hämta id:et
+    let number = 'LIG' + req.params.number
+
+    //Hämtar alla devices från vår databas "db"
+    db.get('devices')
+    //Hittar det specifika ID:et vi får från vår number variabel som hämtas info från params
+    .find({id: number })
+    //Tilldelar true/false till lampa som man får från params.
+    .assign({ on : req.params.status })
+    //Value gör så vi kan läsa datan vi får tillbaka
+    .value();
+
+    //Uppdaterar med den nya infon från assign
+    update();
+})
+
+//Brightness
+
+app.get('/lights/:number/brightness/:bright', (req,res) => {
+
+    //vårt nummer från params som kommer sättas tillsammans med "LIG" för att hämta id:et
+    let number = 'LIG' + req.params.number
+
+    //Hämtar alla devices från vår databas "db"
+    db.get('devices')
+    //Hittar det specifika ID:et vi får från vår number variabel som hämtas info från params
+    .find({id: number })
+    //Tilldelar brightness till lampa som man får från params. Mellan 0-1
+    .assign({ brightness : req.params.bright })
+    //Value gör så vi kan läsa datan vi får tillbaka
+    .value();
+
+    //Uppdaterar med den nya infon från assign
+    update();
+    //Får tillbaka meddelande om att lampan har ljusstyrkan som hämtas från från params.
+    res.send(`Lampa ${req.params.number} har ljusstyrka: ${req.params.bright}`)
+})
+
+
+
+/****************** DÖRR ****************/
+
+//Open/locked
+
+app.get('/lock/status/:status', (req,res) => {
+    //om status sätts som open i URL:en kommer dörren att öppnas med hjälp av params. Skriver man locked i URL:en kommer dörren att låsas, skriver man något annat kommer man få tillbaka ett meddelande om att skriva "on" eller "off"
+    if(req.params.status === "open"){
+        req.params.status = true
+        res.send("Upplåst")
+    } else if(req.params.status === "locked"){
+        req.params.status = false
+        res.send("Låst")
+    } else{
+        res.send("Välj rätt kommando, 'on' eller 'off'")
+    }
+
+    //Hämtar alla devices från vår databas "db"
+    db.get('devices')
+    //Hittar det specifika ID:et "LOC1"
+    .find({id: "LOC1" })
+    //Tilldelar true/false till dörren som man får från params.
+    .assign({ locked : req.params.status }) 
+    //Value gör så vi kan läsa datan vi får tillbaka
+    .value();
+
+    //Uppdaterar med den nya infon från assign
+    update();
 })
